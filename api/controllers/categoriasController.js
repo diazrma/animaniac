a13d25d5b367b8d26e6615e7d83e03347be6566c
Rodrigo Cardoso <diazrma@gmail.com>
2020-03-22 20:29:38 -0300

    'categorias'

--- api/controllers/categoriasController.js
+++ api/controllers/categoriasController.js
@@ -1,10 +1,27 @@
 'use strict'
 
-const db = require('../config');
+const conexao = require('../config');
 
-exports.get = async(req, res, next) => {
+exports.get = async (req, res, next) => {
 
-db.executaQuery('SELECT * FROM usuarios',res);
+    conexao.query(`SELECT cod_categoria, nome ,icone, capa ,visualizacao FROM categorias`, function (error, results, fields) {
+        if (error) {
+            res.status(400).send(error);
+
+        }
+        else {
+
+
+
+
+            res.status(200).send({ results });
+
+        }
+
+    });
+
+
+
 
 }
 
--- api/server.js
+++ api/server.js
@@ -35,6 +35,8 @@ app.use(function (req, res, next) {
     next();
 });
 
+app.use('/assets', express.static(__dirname + '/assets'));
+
 app.use('/', indexRoute);
 app.use('/cadastro', cadastroRoute);
 app.use('/login', loginRoute);
